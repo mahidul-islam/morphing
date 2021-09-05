e91c1d4a653cc09787ac186d19b35a0d0072a4b7
mahidul-islam <mizihan84@gmail.com>
2021-09-05 16:12:45 +0600

    code working

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,50 @@ void main() async {
   runApp(HistoryApp());
 }
 
+// For testing the link with firebase.
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'History Client',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('History Client App'),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () => FirebaseFirestore.instance
+//               .collection('testing')
+//               .add({'timestamp': Timestamp.fromDate(DateTime.now())}),
+//           child: Icon(Icons.add),
+//         ),
+//         body: StreamBuilder(
+//           stream: FirebaseFirestore.instance.collection('testing').snapshots(),
+//           builder:
+//               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+//             if (!snapshot.hasData) {
+//               return const SizedBox.shrink();
+//             }
+//             return ListView.builder(
+//               itemCount: snapshot.data?.docs.length ?? 0,
+//               itemBuilder: (BuildContext context, int index) {
+//                 final docData = snapshot.data?.docs[index].data();
+//                 // final dateTime = (docData!['timestamp']! as Timestamp).toDate();
+//                 return ListTile(
+//                   title: Text(docData.toString()),
+//                 );
+//               },
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
